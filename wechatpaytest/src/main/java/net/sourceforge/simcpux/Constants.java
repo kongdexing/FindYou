6667c3a68446ef5fd3c95c7783b832c8e0ed41e5
kongdexing <il>
2017-04-11 18:08:52 +0800

    新增全局返回控件

---
+++ wechatpaytest/src/main/java/net/sourceforge/simcpux/Constants.java
@@ -0,0 +1,12 @@
+package net.sourceforge.simcpux;
+
+public class Constants {
+    // APP_ID �滻Ϊ���Ӧ�ôӹٷ���վ���뵽�ĺϷ�appId
+    public static final String APP_ID = "wx1af4f660ce9e6b37";
+
+    public static class ShowMsgActivity {
+        public static final String STitle = "showmsg_title";
+        public static final String SMessage = "showmsg_message";
+        public static final String BAThumbData = "showmsg_thumb_data";
+    }
+}
