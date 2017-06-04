7d76140b8e5037358913b1674ca07587c3b22a48
kongdexing <171024402@qq.com>
2017-06-04 23:12:38 +0800

    fix pole 12 15

--- electricline/src/main/java/com/dexing/electricline/ui/DrawLineActivity.java
+++ electricline/src/main/java/com/dexing/electricline/ui/DrawLineActivity.java
@@ -26,6 +26,12 @@ import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.LatLngBounds;
 import com.amap.api.maps2d.model.Marker;
 import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.maps2d.overlay.PoiOverlay;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.core.SuggestionCity;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
 import com.dexing.electricline.R;
 import com.dexing.electricline.model.EPoint;
 import com.dexing.electricline.model.Help;
@@ -46,8 +52,8 @@ import cn.bmob.v3.listener.FindListener;
 import cn.bmob.v3.listener.SaveListener;
 import cn.bmob.v3.listener.UpdateListener;
 
-public class DrawLineActivity extends BaseLineActivity implements AMap.OnMapClickListener, AMap.OnMarkerClickListener,
-        AMap.OnInfoWindowClickListener {
+public class DrawLineActivity extends BaseLineActivity implements AMap.OnMapClickListener,
+        AMap.OnMarkerClickListener, AMap.OnInfoWindowClickListener, PoiSearch.OnPoiSearchListener {
 
     private String TAG = DrawLineActivity.class.getSimpleName();
     private AMap aMap;
@@ -67,6 +73,9 @@ public class DrawLineActivity extends BaseLineActivity implements AMap.OnMapClic
 
     @BindView(R.id.spinnerPoint)
     Spinner spinnerPoint;
+    private PoiResult poiResult; // poi返回的结果
+    private PoiSearch.Query query;// Poi查询条件类
+    private PoiSearch poiSearch;// POI搜索
 
     private int point_type = TYPE_POLE_12;
 
@@ -110,6 +119,12 @@ public class DrawLineActivity extends BaseLineActivity implements AMap.OnMapClic
             @Override
             public void done(List<EPoint> list, BmobException e) {
                 progress.setVisibility(View.GONE);
+                if (list.size() == 0) {
+                    Log.i(TAG, "done: point size is 0");
+                    //检索
+                    doSearchQuery();
+                    return;
+                }
                 for (int i = 0; i < list.size(); i++) {
                     EPoint point = list.get(i);
                     LatLng latLng = point.getLatLng();
@@ -121,6 +136,67 @@ public class DrawLineActivity extends BaseLineActivity implements AMap.OnMapClic
                 aMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20));
             }
         });
+
+    }
+
+    /**
+     * 开始进行poi搜索
+     */
+    protected void doSearchQuery() {
+        Log.i(TAG, "doSearchQuery: ");
+        query = new PoiSearch.Query(currentVillage.getName(), "", "山东");// 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
+        query.setPageSize(10);// 设置每页最多返回多少条poiitem
+        query.setPageNum(0);// 设置查第一页
+        query.setCityLimit(true);
+
+        poiSearch = new PoiSearch(this, query);
+        poiSearch.setOnPoiSearchListener(this);
+        poiSearch.searchPOIAsyn();
+    }
+
+
+    /**
+     * POI信息查询回调方法
+     */
+    @Override
+    public void onPoiSearched(PoiResult result, int rCode) {
+        Log.i(TAG, "onPoiSearched: " + rCode);
+        if (rCode == AMapException.CODE_AMAP_SUCCESS) {
+            if (result != null && result.getQuery() != null) {// 搜索poi的结果
+                if (result.getQuery().equals(query)) {// 是否是同一条
+                    poiResult = result;
+                    // 取得搜索到的poiitems有多少页
+                    List<PoiItem> poiItems = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
+                    List<SuggestionCity> suggestionCities = poiResult
+                            .getSearchSuggestionCitys();// 当搜索不到poiitem数据时，会返回含有搜索关键字的城市信息
+                    Log.i(TAG, "onPoiSearched: poiItems size " + poiItems.size());
+                    Log.i(TAG, "onPoiSearched: suggestionCities size " + suggestionCities.size());
+
+                    if (poiItems != null && poiItems.size() > 0) {
+                        aMap.clear();// 清理之前的图标
+                        PoiOverlay poiOverlay = new PoiOverlay(aMap, poiItems);
+                        poiOverlay.removeFromMap();
+                        poiOverlay.addToMap();
+                        poiOverlay.zoomToSpan();
+                    } else if (suggestionCities != null
+                            && suggestionCities.size() > 0) {
+                        showSuggestCity(suggestionCities);
+                    } else {
+//                        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            } else {
+//                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+//            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem item, int rCode) {
+        // TODO Auto-generated method stub
 
     }
 
@@ -324,6 +400,19 @@ public class DrawLineActivity extends BaseLineActivity implements AMap.OnMapClic
         markerView.setPointNum(point.getNumber());
         markerOption.icon(BitmapDescriptorFactory.fromView(markerView));
         return markerOption;
+    }
+
+    /**
+     * poi没有搜索到数据，返回一些推荐城市的信息
+     */
+    private void showSuggestCity(List<SuggestionCity> cities) {
+        String infomation = "推荐城市\n";
+        for (int i = 0; i < cities.size(); i++) {
+            infomation += "城市名称:" + cities.get(i).getCityName() + "城市区号:"
+                    + cities.get(i).getCityCode() + "城市编码:"
+                    + cities.get(i).getAdCode() + "\n";
+        }
+//        ToastUtil.show(PoiKeywordSearchActivity.this, infomation);
     }
 
     /**
